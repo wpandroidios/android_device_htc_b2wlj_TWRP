f23860abe5b31552b21ca8508dba8942d4e444e4
Captain Throwback <captainthrowback@hotmail.com>
2014-09-26 18:25:56 -0400

    Add extension to board name for multirom app recognition

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,7 @@ BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --board mrom$(shell date -u +%Y%m%d)
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --board mrom$(shell date -u +%Y%m%d)-01
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/m8/mkbootimg.mk
 
 TARGET_KERNEL_CONFIG := m8_defconfig
