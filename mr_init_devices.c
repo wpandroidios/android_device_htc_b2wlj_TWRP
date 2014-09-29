cd3ebcbd695a1f5bcadb1e6eb047ed58359dfc13
Captain Throwback <captainthrowback@hotmail.com>
2014-09-29 11:07:16 -0400

    multirom: don't initialize the full external block device
    Only intialize the partition(s)

--- mr_init_devices.c
+++ mr_init_devices.c
@@ -13,7 +13,6 @@ const char *mr_init_devices[] =
     "/sys/class/graphics/fb0",
 
     "/sys/block/mmcblk0",
-    "/sys/block/mmcblk1",
     "/sys/devices/msm_sdcc.1",
     "/sys/devices/msm_sdcc.1/mmc_host/mmc0",
     "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001",
@@ -25,6 +24,7 @@ const char *mr_init_devices[] =
     "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p47",
     "/sys/devices/msm_sdcc.1/mmc_host/mmc0/mmc0:0001/block/mmcblk0/mmcblk0p48",
     "/sys/devices/msm_sdcc.2/mmc_host/mmc1/mmc1:e624/block/mmcblk1/mmcblk1p1",
+    "/sys/devices/msm_sdcc.2/mmc_host/mmc1/mmc1:e624/block/mmcblk1/mmcblk1p2",
 
     "/sys/bus/mmc",
     "/sys/bus/mmc/drivers/mmcblk",
