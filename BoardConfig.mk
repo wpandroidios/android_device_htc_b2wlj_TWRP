df0adbbde729b823999b808b9760428d466967af
Captain Throwback <captainthrowback@hotmail.com>
2014-12-04 13:09:36 -0500

    Add custom kernel toolchain for kernel build
    kernel wants gcc-4.7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,7 +55,7 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 --tags_offset 0x01e00000 --board mrom$(shell date -u +%Y%m%d)-01
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/m8/mkbootimg.mk
-
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
 TARGET_KERNEL_CONFIG := m8_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/m8gpe
 
