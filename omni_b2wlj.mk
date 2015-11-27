1567c6b1e6ba645b57dfc1f0bf4befb9bb73cfbd
android <android>
2015-11-27 18:23:41 +0900

    adapt to b2wlj

--- omni_b2wlj.mk
+++ omni_b2wlj.mk
@@ -2,16 +2,16 @@
 $(call inherit-product, vendor/omni/config/cdma.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/m8/full_m8.mk)
+$(call inherit-product, device/htc/b2wlj/full_b2wlj.mk)
 # Release name
-PRODUCT_RELEASE_NAME := m8
+PRODUCT_RELEASE_NAME := b2wlj
 
 # Inherit some common DU stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := m8
-PRODUCT_NAME := omni_m8
+PRODUCT_DEVICE := b2wlj
+PRODUCT_NAME := omni_b2wlj
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := m8
+PRODUCT_MODEL := b2wlj
 PRODUCT_MANUFACTURER := htc
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,2 @@
-add_lunch_combo omni_m8-userdebug
-add_lunch_combo omni_m8-eng
+add_lunch_combo omni_b2wlj-userdebug
+add_lunch_combo omni_b2wlj-eng
